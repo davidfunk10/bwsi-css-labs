90b77595a524119494136e561357ecff94cc2f13
davidfunk10 <davyfunk10@gmail.com>
2026-03-22 17:55:01 -0400

    Add personal introduction print to lab_1a

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,7 @@ def main():
     name = "David Funk" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
+    print("Hi everyone! My name is David Funk, and I am a sophomore at Christ Church Episcopal School in Greenville, South Carolina. A fun fact about me is that I speak Mandarin, and I am very excited to start this course!")
 
 if __name__ == "__main__":
     main()
